800798d868fd8874bffd4598828eb3f44a59259f
Nathan Maloney <nathan25maloney@gmail.com>
2017-08-05 10:47:06 -0700

    added the basic server and handlebars layout

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var path = require("path");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+var db = require("./models");
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+db.sequelize.sync().then(function() {
+  app.listen(port, function() {
+    console.log("App listening on PORT " + port);
+  });
+});
