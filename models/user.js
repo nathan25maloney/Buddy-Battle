06490a0997041c6609921c77c2afe7695876889e
Catherine Liu <catherineqliu@outlook.com>
2017-08-06 22:25:30 -0700

    Created UserChallenge and Score models. Updated fields and associations for each model.

--- models/user.js
+++ models/user.js
@@ -1,17 +1,41 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    name: DataTypes.STRING,
-    pass: DataTypes.STRING,
-    	
-    });
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
 
   User.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
-    User.hasMany(models.challenges, {
+    // Associating User with Challenge they create
+    // When an User is deleted, also delete any associated Challenges
+    User.hasMany(models.Challenge, {
+      foreignKey: "creator_id",
       onDelete: "cascade"
     });
+
+    // Associating User with Score
+    // When an User is deleted, also delete any associated Score
+    User.hasMany(models.Score, {
+      foreignKey: "user_id",
+      onDelete: "cascade"
+    });
+
+    // Assocating User with Challenge as participant in challenge through UserChallenge
+    User.belongsToMany(models.Challenge, { 
+      as: "challenge",
+      through: models.UserChallenge,
+      foreignKey: {
+        name: "user_id",
+        allowNull: false
+      }
+    });
   };
+
   return User;
 };
 
