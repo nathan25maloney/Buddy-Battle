1cf6858ff0d7f878798c6fe81b73c8c33227b395
Catherine Liu <catherineqliu@outlook.com>
2017-08-06 22:31:07 -0700

    Added name to User model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     username: {
       type: DataTypes.STRING,
       allowNull: false
