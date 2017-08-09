ec9bf581ee65f32e53311717ad60a27a179eb7d3
Catherine Liu <catherineqliu@outlook.com>
2017-08-08 22:11:47 -0700

    Uncommented settings in index.js in models.

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
-// var settings  = require(__dirname +'/../settings/settings.js');
+var settings  = require(__dirname +'/../settings/settings.js');
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
