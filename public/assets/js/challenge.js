b40e92bdb84cb94d5cf1034d2c5a990003493e8b
Catherine Liu <catherineqliu@outlook.com>
2017-08-10 21:04:46 -0700

    updated challenge handlebar

--- public/assets/js/challenge.js
+++ public/assets/js/challenge.js
@@ -71,22 +71,22 @@ span.onclick = function() {
 }
 
 
-//JQuery For Progress Bar//
-$(document).ready(function() {
-    var progresspump = setInterval(function() {
-        /* query the completion percentage from the server */
-        $.get("/app/update-progress", function(data) {
-            /* update the progress bar width */
-            $("#progress").css('width', data + '%');
-            /* and display the numeric value */
-            $("#progress").html(data + '%');
+// //JQuery For Progress Bar//
+// $(document).ready(function() {
+//     var progresspump = setInterval(function() {
+//         /* query the completion percentage from the server */
+//         $.get("/app/update-progress", function(data) {
+//             /* update the progress bar width */
+//             $("#progress").css('width', data + '%');
+//              and display the numeric value 
+//             $("#progress").html(data + '%');
 
-            /* test to see if the job has completed */
-            if (data > 99.999) {
-                clearInterval(progresspump);
-                $("#progressouter").removeClass("active");
-                $("#progress").html("Done");
-            }
-        })
-    }, 1000);
-});
+//             /* test to see if the job has completed */
+//             if (data > 99.999) {
+//                 clearInterval(progresspump);
+//                 $("#progressouter").removeClass("active");
+//                 $("#progress").html("Done");
+//             }
+//         })
+//     }, 1000);
+// });
