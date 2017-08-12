0c06b493514a0e726c3f94374e94681da311bd7c
Nathan Maloney <nathan25maloney@gmail.com>
2017-08-11 23:04:43 -0700

    fully working site

--- models/index.js
+++ models/index.js
@@ -7,12 +7,12 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
-var settings  = require(__dirname +'/../settings/settings.js');
+// var settings  = require(__dirname +'/../settings/settings.js');
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, settings[0].pass, config);
+  var sequelize = new Sequelize(config.database, config.username, config.pass, config);
 }
 
 fs
